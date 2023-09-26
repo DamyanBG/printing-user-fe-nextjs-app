d4d8092640aebdff513766dc33dcf674c3170520
DamyanBG <d.dimitrov.1988@abv.bg>
2023-09-26 15:23:38 +0300

    hamburger menu

--- layout/layout.tsx
+++ layout/layout.tsx
@@ -1,28 +1,35 @@
 import Link from "next/link";
-import Image from 'next/image'
+import Image from "next/image";
 
 import { LayoutProps } from "@/types/propTypes";
-import classes from "./header.module.css"
+import classes from "./header.module.css";
 
 function Layout(props: LayoutProps) {
     return (
         <>
             <header className={classes.header}>
                 <Link href="/">
-                    <div style={{
-                        display: "flex"
-                    }}>
+                    <div
+                        style={{
+                            display: "flex",
+                            alignItems: "center"
+                        }}
+                    >
                         <Image
-                            src='/images/3d-print-w.png'
-                            alt='Logo'
+                            src="/images/3d-print-w.png"
+                            alt="Logo"
                             width={40}
                             height={40}
                         />
                         <h1>3D PRINTING WORKSHOP</h1>
                     </div>
                 </Link>
-                <nav className={classes.navWide}>
-                    <ul>
+                <input id="menu-toggle" type="checkbox" />
+                <label className="menu-button-container" htmlFor="menu-toggle">
+                    <div className="menu-button"></div>
+                </label>
+                
+                    <ul className="menu">
                         <li>
                             <Link href="/samples">Примерни продукти</Link>
                         </li>
@@ -33,7 +40,7 @@ function Layout(props: LayoutProps) {
                             <Link href="/contact">Контакт</Link>
                         </li>
                     </ul>
-                </nav>
+                
             </header>
             <main>{props.children}</main>
         </>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import Layout from '@/layout/layout'
 import '@/styles/globals.css'
 import '@/styles/main.css'
+import '@/styles/hamburger.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
